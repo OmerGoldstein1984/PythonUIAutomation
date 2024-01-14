69185a983d44bc46b3263d3f5ba7b82e346ad7cf
OmerPc <omer.goldstein.1984@gmail.com>
2024-01-14 02:01:34 +0200

    added run on linux get constants from config in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
+import sys
+from sys import platform
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
-
 import allure
 import pytest
 from selenium import webdriver
@@ -31,17 +32,36 @@ def pytest_runtest_makereport(item, call):
             print(f"Failed to attach screenshot: {e}")
 
 
+def isTestsRunningOnLinux():
+    if not sys.platform.startswith('win'):
+        return True
+    return False
+
+
 @pytest.fixture(scope="function", autouse=True)
 def setup(request):
     global driver
+    firefoxoptions = webdriver.FirefoxOptions()
+    chromeoptions = webdriver.ChromeOptions()
+    firefoxoptions.add_argument("--headless")
+    chromeoptions.add_argument("--headless")
     browsername = request.config.getoption("--browsername")
     env = request.config.getoption("--env")
     site_url = utils.read_config(env, "url")
-    if browsername == "ch":
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    elif browsername == "ff":
-        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+    if isTestsRunningOnLinux() is True:
+        if browsername == "ch":
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chromeoptions)
+        elif browsername == "ff":
+            driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=firefoxoptions)
+
+    if isTestsRunningOnLinux() is False:
+        if browsername == "ch":
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        elif browsername == "ff":
+            driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     driver.get(site_url)
+    request.cls.env=env
     request.cls.driver = driver
     yield
     driver.quit()
--- tests/test_login.py
+++ tests/test_login.py
@@ -2,27 +2,34 @@ import allure
 import pytest
 
 from pages.LoginPage import LoginPage
+from utils.utils import utils
+
 
 @pytest.mark.usefixtures("setup")
 @allure.feature("login page")
 @allure.severity(allure.severity_level.CRITICAL)
 @allure.story("login page")
 class Testlogin:
+
     @allure.description("This test is to test the login page valid credentials")
     def test_valid_login(self):
+        user=utils.read_config(self.env,'user')
+        password=utils.read_config(self.env,'password')
         login = LoginPage(self.driver)
-        login.enterCredentials("Admin", "admin123")
+        login.enterCredentials(user, password)
         assert login.isError() is False
 
     @allure.description("This test is to test the login page invalid credentials")
     def test_invalid_login(self):
+        user=utils.read_config(self.env,'user')
         login = LoginPage(self.driver)
-        login.enterCredentials("Admin123", "admin123")
+        login.enterCredentials(user+"123", "123")
         assert login.isError() is True
     @allure.description("This test is to test the dashboard page contains the right user name")
     def test_invalid_user_displayed(self):
+        user=utils.read_config(self.env,'user')
+        password=utils.read_config(self.env,'password')
         login = LoginPage(self.driver)
-        login.enterCredentials("Admin", "admin123")
-        assert login.isError() is False
+        login.enterCredentials(user, password)
         dashboard = login.goToDashboard()
         assert dashboard.getUserName() == "Admin"
